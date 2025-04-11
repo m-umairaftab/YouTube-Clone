19334a12cf98062cff33c4ec705f1cb18463780c
Umair Aftab <m.umair.umairAftab@gmail.com>
2025-04-11 13:08:22 +0500

    youtube homw page

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Body from "./components/Body";
+import Head from "./components/Head";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./utils/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
+function App() {
+  return (
+    <Provider store={store}>
+      <Head />
+      <RouterProvider router={appRouter} />
+    </Provider>
+  );
+}
+
+export default App;
